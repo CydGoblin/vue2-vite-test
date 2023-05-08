eed54a13551eac6814317c37f00cf8e069ce3a70
Luz Virginia Aramburo Alvarez <luzaramburo13@gmail.com>
2023-05-08 12:05:49 -0700

    ag grid test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,37 @@
-<script setup>
-</script>
-
 <template>
-  <div>
-      Hi
-  </div>
+    <ag-grid-vue
+            style="width: 500px; height: 200px"
+            class="ag-theme-alpine"
+            :columnDefs="columnDefs"
+            :rowData="rowData"
+    >
+    </ag-grid-vue>
 </template>
+
+<script>
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import { AgGridVue } from "ag-grid-vue";
+import Rating from "./Rating.vue";
+
+export default {
+    name: "App",
+    components: {
+        AgGridVue,
+        Rating,
+    },
+    setup() {
+        return {
+            columnDefs: [
+                { headerName: "Make", field: "make" },
+                { headerName: "Rating", field: "rating", cellRenderer: "Rating" },
+            ],
+            rowData: [
+                { make: "Toyota", rating: 4 },
+                { make: "Ford", rating: 1 },
+                { make: "Porsche", rating: 5 },
+            ],
+        };
+    },
+};
+</script>
---
+++ src/Rating.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  setup(props) {
+    const displayValue = new Array(props.params.value).fill("#").join("");
+    return {
+      displayValue,
+    };
+  },
+};
+</script>
+
+<template>
+    <span>{{ displayValue }} HELLO</span>
+</template>
